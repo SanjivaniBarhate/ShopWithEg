571b80a14b3c06f5128a61898a028d207789a8be
SanjivaniBarhate <sanjivanibarhate24@gmail.com>
2023-06-16 10:01:52 +0530

    added runner class

--- src/main/java/pages/LandingPage.java
+++ src/main/java/pages/LandingPage.java
@@ -1,5 +1,26 @@
 package pages;
 
-public class LandingPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utility.DriverFactory;
+
+public class LandingPage extends DriverFactory{
+	
+	public LandingPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//p[text()='login']")
+	public WebElement loginButton;
+	
+	public void clickLoginButton()
+	{
+		loginButton.click();
+		
+	}
 
 }
